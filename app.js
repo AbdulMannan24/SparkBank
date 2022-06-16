f62b51160d34d5d5937d21d87b9c4d75586014de
Mohd Abdul Mannan <abdulmannan@Mohds-MacBook-Air.local>
2022-06-16 15:52:33 +0530

    database deactivated

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-const mysql = require("mysql2");
+//const mysql = require("mysql2");
 //const alert = require('alert');
 
 
 // db definition
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'learnsql',
-    database: 'Bank'
-})
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'learnsql',
+//     database: 'Bank'
+// })
 
 
 // db connection
-connection.connect(function(){
-    console.log("MySql connected.....");
-});
+// connection.connect(function(){
+//     console.log("MySql connected.....");
+// });
 const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -32,27 +32,27 @@ let his = [];
 let his1 = [];
 let his2 = [];
 
-connection.query('select * from Transfers', function(err,result) {
-    result.forEach(element=> {
-        his.push(element.sender);
-        his1.push(element.receiver);
-        his2.push(element.cash);
-    })
-});
-connection.query('select * from Customers', function(err, result) {
-    if (err) throw err;
-   // let data = JSON.parse(result)
-   // console.log (data);
-    result.forEach(element => {
-        let name = element.name;
-        let ema = element.email;
-        let balance = element.AccountBalance;
-        cust.push(name);
-        email.push(ema);
-        cash.push(balance);
-       // console.log(balance);
-    });
-})
+// connection.query('select * from Transfers', function(err,result) {
+//     result.forEach(element=> {
+//         his.push(element.sender);
+//         his1.push(element.receiver);
+//         his2.push(element.cash);
+//     })
+// });
+// connection.query('select * from Customers', function(err, result) {
+//     if (err) throw err;
+//    // let data = JSON.parse(result)
+//    // console.log (data);
+//     result.forEach(element => {
+//         let name = element.name;
+//         let ema = element.email;
+//         let balance = element.AccountBalance;
+//         cust.push(name);
+//         email.push(ema);
+//         cash.push(balance);
+//        // console.log(balance);
+//     });
+// })
 // home page
 
 app.get('/', function(req,res) {
@@ -60,20 +60,20 @@ app.get('/', function(req,res) {
     cust =[];
     email = [];
     cash = [];
-    connection.query(sql, function(err, result) {
-            if (err) throw err;
-           // let data = JSON.parse(result)
-           // console.log (data);
-            result.forEach(element => {
-                let name = element.name;
-                let ema = element.email;
-                let balance = element.AccountBalance;
-                cust.push(name);
-                email.push(ema);
-                cash.push(balance);
-               // console.log(balance);
-            });
-    })
+    // connection.query(sql, function(err, result) {
+    //         if (err) throw err;
+    //        // let data = JSON.parse(result)
+    //        // console.log (data);
+    //         result.forEach(element => {
+    //             let name = element.name;
+    //             let ema = element.email;
+    //             let balance = element.AccountBalance;
+    //             cust.push(name);
+    //             email.push(ema);
+    //             cash.push(balance);
+    //            // console.log(balance);
+    //         });
+    // })
     res.render('home');
 })
 
@@ -98,70 +98,70 @@ app.post('/transfer', function(req,res) {
     let amount = Number(req.body.amount);
     let ssql = 'select * from Customers where name = ' + '"' + sender + '"'; 
     let sbalance = 0;
-    connection.query(ssql, function(err, result){
-        result.forEach(element => {
-            if (sender === element.name) {
-                let cur = Number(element.AccountBalance);
-                sbalance = Number(cur-amount);
-                console.log(cur);
-                console.log(sbalance);
-            }
-        }) 
-    });
+    // connection.query(ssql, function(err, result){
+    //     result.forEach(element => {
+    //         if (sender === element.name) {
+    //             let cur = Number(element.AccountBalance);
+    //             sbalance = Number(cur-amount);
+    //             console.log(cur);
+    //             console.log(sbalance);
+    //         }
+    //     }) 
+    // });
 
-    let rsql = 'select * from Customers where name = ?';
-    let rbalance = 0;
-    connection.query(rsql,[receiver], function(err, result) {
-        result.forEach(element=> {
-            if (receiver === element.name) {
-                let cur = Number(element.AccountBalance);
-                rbalance = amount+cur;
-                console.log(cur);
-                console.log(rbalance);
-            }
-        })
+    // let rsql = 'select * from Customers where name = ?';
+    // let rbalance = 0;
+    // connection.query(rsql,[receiver], function(err, result) {
+    //     result.forEach(element=> {
+    //         if (receiver === element.name) {
+    //             let cur = Number(element.AccountBalance);
+    //             rbalance = amount+cur;
+    //             console.log(cur);
+    //             console.log(rbalance);
+    //         }
+    //     })
     
-    })
-     //just update sbalance and rbalance tomorrow
-     setTimeout(()=>{
-        let upcust1 = 'update Customers set AccountBalance = ? where name = ?';
-    connection.query(upcust1,[sbalance,sender],function(err,result,fields) {
-        if (err) throw err;
-        console.log('sender balance updated');
-    }) 
+    // })
+    //  //just update sbalance and rbalance tomorrow
+    //  setTimeout(()=>{
+    //     let upcust1 = 'update Customers set AccountBalance = ? where name = ?';
+    // connection.query(upcust1,[sbalance,sender],function(err,result,fields) {
+    //     if (err) throw err;
+    //     console.log('sender balance updated');
+    // }) 
 
-    let upcust2 = `update Customers set AccountBalance = ? where name = ?`;
-    connection.query(upcust2,[rbalance, receiver],function(err,result,fields) {
-        if (err) throw err;
-        console.log('receiver balance updated');
-    })
-        // his.push(sender);
-        // his1.push(receiver);
-        // his2.push(amount);
-    },5000);
+    // let upcust2 = `update Customers set AccountBalance = ? where name = ?`;
+    // connection.query(upcust2,[rbalance, receiver],function(err,result,fields) {
+    //     if (err) throw err;
+    //     console.log('receiver balance updated');
+    // })
+    //     // his.push(sender);
+    //     // his1.push(receiver);
+    //     // his2.push(amount);
+    // },5000);
     
-    setTimeout(()=>{
-        let trans = 'insert into transfers values(?,?,?)';
-        connection.query(trans,[sender,receiver,amount], function(err,result) {
-            if (err) throw err;
-            console.log("transferred succesfully");
-            // his.push(sender);
-            // his1.push(receiver);
-            // his2.push(amount);
-        })
-    },6000)
+    // setTimeout(()=>{
+    //     let trans = 'insert into transfers values(?,?,?)';
+    //     connection.query(trans,[sender,receiver,amount], function(err,result) {
+    //         if (err) throw err;
+    //         console.log("transferred succesfully");
+    //         // his.push(sender);
+    //         // his1.push(receiver);
+    //         // his2.push(amount);
+    //     })
+    // },6000)
     setTimeout(()=> {
         his = [];
         his1 = [];
         his2 = [];
         
-        connection.query('select * from Transfers', function(err,result) {
-            result.forEach(element=> {
-                his.push(element.sender);
-                his1.push(element.receiver);
-                his2.push(element.cash);
-            })
-        });
+        // connection.query('select * from Transfers', function(err,result) {
+        //     result.forEach(element=> {
+        //         his.push(element.sender);
+        //         his1.push(element.receiver);
+        //         his2.push(element.cash);
+        //     })
+        // });
        
     },7000)
     console.log(sender);
